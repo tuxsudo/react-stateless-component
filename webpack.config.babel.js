a4395c8a92f7c1033753fec3c4195fda60f7abad
Jared Anderson <jared@tuxsudo.com>
2016-02-18 15:25:30 -0700

    remove unnecessary comments

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,6 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import precss from 'precss';
 import {tmpdir} from 'os';
 import {sync as globSync} from 'glob';
-import path from 'path';
 import {name as pkgName} from './package.json';
 
 const base = {
@@ -88,83 +87,3 @@ export default {
     ...base,
     ...(process.env.NODE_ENV === "test" ? test : demo)
 };
-
-
-
-
-
-
-
-
-
-
-
-// export default {
-//     entry: "./demo/demo.js",
-//     output: {
-//         path: './demo',
-//         filename: "bundle.js"
-//     },
-//     module: {
-//         loaders: [
-//             {
-//                 test: /\.css$/,
-//                 loader: ExtractTextPlugin.extract(
-//                     "style-loader",
-//                     "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader"
-//                     // "sass-loader"
-//                 )
-//             },
-//
-//
-//
-//             {
-//                 test : /\.jsx?$/,
-//                 exclude : /(node_modules|bower_components)/,
-//                 loader  : 'babel',
-//                 query: {
-//                     presets: ['react', 'es2015']
-//                 }
-//             }
-//
-//         ]
-//
-//     },
-//
-//     plugins: [
-//         new ExtractTextPlugin("styles.css")
-//     ],
-//
-//     cssLoader: {
-//         modules: true
-//     },
-//
-//     postcss : [
-//
-//         precss(),
-//
-//         cssnano({
-//             autoprefixer : {
-//                 add      : true,
-//                 remove   : true,
-//                 browsers : ['last 2 versions']
-//             },
-//
-//             discardComments : {
-//                 removeAll : true
-//             }
-//
-//         })
-//     ],
-//
-//     devServer: {
-//         contentBase: './demo',
-//         historyApiFallback: true,
-//         hot: false,
-//         inline: true,
-//         progress: true,
-//         compress: true
-//     }
-//
-//
-// };
